0c5ae7c63989b3693961bf3737a5ecc988878c93
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-04-08 04:50:28 +0700

    !shib channel

---
+++ src/cogs/shib.ts
@@ -0,0 +1,28 @@
+import { IModule } from "../types/ModuleLoader";
+import { Plugin } from "./Plugin";
+import { Message } from "discord.js"; 
+import { inChannel, shouldHaveAuthor } from "./checks/commands";
+
+class SHIBCHANNEL extends Plugin implements IModule {
+
+    constructor() {
+        super({
+            "message": (msg:Message) => this.onMessage(msg),
+            "messageUpdate": (old, newMsg:Message) => this.onMessage(newMsg)
+        });
+    }
+
+    @inChannel("300019335055802368")
+    @shouldHaveAuthor
+    async onMessage(msg:Message) {
+        if(msg.author.id === "235849760253280257") { return; }
+        if(msg.content !== "!shib" && msg.attachments.size < 1) { msg.delete(); }
+    }
+
+    async unload() {
+        this.unhandleEvents();
+        return true;
+    }
+}
+
+module.exports = SHIBCHANNEL;
