707521e75b5e7c262a0b47259512cc918c13b2ab
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-04-08 14:04:05 +0700

    travis: test?

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="modules/bluebird/index.d.ts" />
 /// <reference path="modules/knex/index.d.ts" />
 /// <reference path="modules/node-fetch/index.d.ts" />
+/// <reference path="modules/discord.js/index.d.ts" />
