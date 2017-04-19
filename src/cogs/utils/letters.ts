1515bfa16dc3f8e252f8fcce99fab9c7a0593f2e
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-04-20 06:57:49 +0700

    letters: replaced numbers to unicode emojis

--- src/cogs/utils/letters.ts
+++ src/cogs/utils/letters.ts
@@ -2,16 +2,16 @@ export function convertNumbers(number:string|number) {
     let str = number + "";
     str = str.replace(/([0-9])/ig, (s) => {
         switch(s) {
-            case "0": return ":zero:";
-            case "1": return ":one:";
-            case "2": return ":two:";
-            case "3": return ":three:";
-            case "4": return ":four:";
-            case "5": return ":five:";
-            case "6": return ":six:";
-            case "7": return ":seven:";
-            case "8": return ":eight:";
-            case "9": return ":nine:";
+            case "0": return "0️⃣";
+            case "1": return "1️⃣";
+            case "2": return "2️⃣";
+            case "3": return "3️⃣";
+            case "4": return "4️⃣";
+            case "5": return "5️⃣";
+            case "6": return "6️⃣";
+            case "7": return "7️⃣";
+            case "8": return "8️⃣";
+            case "9": return "9️⃣";
             default: return s;
         }
     });
