2fd0f757c5907cd7642712c3d3bee984a3389556
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-04-20 12:38:16 +0700

    letters: don't even ask

--- src/cogs/utils/letters.ts
+++ src/cogs/utils/letters.ts
@@ -1,17 +1,30 @@
+const NUMBERS = [
+  "1⃣ ",
+  "2⃣ ",
+  "3⃣ ",
+  "4⃣ ",
+  "5⃣ ",
+  "6⃣ ",
+  "7⃣ ",
+  "8⃣ ",
+  "9⃣ ",
+  "0⃣"
+];
+
 export function convertNumbers(number:string|number) {
     let str = number + "";
     str = str.replace(/([0-9])/ig, (s) => {
         switch(s) {
-            case "0": return "0️⃣";
-            case "1": return "1️⃣";
-            case "2": return "2️⃣";
-            case "3": return "3️⃣";
-            case "4": return "4️⃣";
-            case "5": return "5️⃣";
-            case "6": return "6️⃣";
-            case "7": return "7️⃣";
-            case "8": return "8️⃣";
-            case "9": return "9️⃣";
+            case "1": return NUMBERS[0];
+            case "2": return NUMBERS[1];
+            case "3": return NUMBERS[2];
+            case "4": return NUMBERS[3];
+            case "5": return NUMBERS[4];
+            case "6": return NUMBERS[5];
+            case "7": return NUMBERS[6];
+            case "8": return NUMBERS[7];
+            case "9": return NUMBERS[8];
+            case "0": return NUMBERS[9];
             default: return s;
         }
     });
