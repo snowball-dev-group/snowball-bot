d316a8724657449d38d71e791fe1514a4a8359e7
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-07-22 02:12:19 +0700

    streamNotifications:twitch: and cleanup here too

--- src/cogs/streamNotifications/services/twitch.ts
+++ src/cogs/streamNotifications/services/twitch.ts
@@ -11,66 +11,50 @@ const TWITCH_USERNAME_REGEXP = /^[a-zA-Z0-9_]{3,24}$/;
 class TwitchStreamingService implements IStreamingService {
     public name = "twitch";
 
-    private clientId:string;
+    private clientId: string;
     private log = getLogger("TwitchStreamingService");
 
-    constructor(clientId:string) {
+    constructor(clientId: string) {
         this.clientId = clientId;
     }
 
-    private streamsCache = new Map<string, {
-        cachedAt:number,
+    private streamsMap = new Map<string, {
+        fetchedAt: number,
         value: ITwitchStream
     }>();
 
-    public async fetch(streamers:IStreamingServiceStreamer[]) {
-        // should return only online streams
-        
-        // don't updating for cached streams
-        let reqDate = Date.now();
-        let notCachedYet = streamers.filter(s => {
-            // is not cached?
-            let cached = this.streamsCache.get(s.uid);
+    public async fetch(streamers: IStreamingServiceStreamer[]) {
+        let cDate = Date.now();
+        let notFetchedYet = streamers.filter(s => {
+            let cached = this.streamsMap.get(s.uid);
             if(!cached) { return true; }
-            if((reqDate - cached.cachedAt) > MAX_STREAM_CACHE_LIFE) { return true; }
+            if((cDate - cached.fetchedAt) > MAX_STREAM_CACHE_LIFE) { return true; }
             return false;
         });
-        
-        let result:IStreamStatus[] = [];
-        
-        if(notCachedYet.length > 0) {
-            let loop = async (toFetch:IStreamingServiceStreamer[], attempt:number) => { 
-                if(attempt > 3) {
-                    throw new StreamingServiceError("TWITCH_TOOMANYATTEMPTS", "Too many attempts. Please, try again later");
-                }
 
-                let resp = await fetch(this.getAPIURL_Streams(toFetch.map(s => s.uid)));
-                if(resp.status === 429) {
-                    await sleep(parseInt(resp.headers.get("retry-after"), 10));
-                    return await loop(toFetch, attempt++);
-                } else if(resp.status !== 200) {
-                    throw new StreamingServiceError("TWITCH_SOMETHINGWRONG", "Something wrong with Twitch. Please, try again later");
-                }
-                
-                let streamsResp = (await resp.json()) as {
+        let result: IStreamStatus[] = [];
+
+        if(notFetchedYet.length > 0) {
+            let processChunk = async (toFetch: IStreamingServiceStreamer[]) => {
+                let streamsResp = (await this.makeRequest(this.getAPIURL_Streams(toFetch.map(s => s.uid)))) as {
                     streams: ITwitchStream[]
                 };
 
                 for(let stream of streamsResp.streams) {
-                    this.streamsCache.set(stream.channel._id + "", {
-                        cachedAt: Date.now(),
+                    this.streamsMap.set(stream.channel._id + "", {
+                        fetchedAt: Date.now(),
                         value: stream
                     });
                 }
             };
-            let chunks = chunk(notCachedYet, 50);
+            let chunks = chunk(notFetchedYet, 50);
             for(let chunk of chunks) {
-                await loop(chunk, 0);
+                await processChunk(chunk);
             }
         }
 
         for(let streamer of streamers) {
-            let cached = this.streamsCache.get(streamer.uid);
+            let cached = this.streamsMap.get(streamer.uid);
             if(!cached || !cached.value) {
                 result.push({
                     status: "offline",
@@ -89,93 +73,73 @@ class TwitchStreamingService implements IStreamingService {
         return result;
     }
 
-    public async getEmbed(stream:IStreamStatus, lang:string) : Promise<IEmbed> {
-        let cachedStream = this.streamsCache.get(stream.streamer.uid);
-        if(!cachedStream) { throw new StreamingServiceError("TWITCH_CACHEFAULT", "Failure"); }
+    public async getEmbed(streamStatus: IStreamStatus, lang: string): Promise<IEmbed> {
+        let stream = this.streamsMap.get(streamStatus.streamer.uid);
+        if(!stream) { throw new StreamingServiceError("TWITCH_CACHEFAULT", "Failure"); }
         return {
             footer: {
                 icon_url: TWITCH_ICON,
                 text: "Twitch"
             },
             description: localizer.getFormattedString(lang, "STREAMING_DESCRIPTION_TWITCH", {
-                username: escapeDiscordMarkdown(cachedStream.value.channel.display_name || cachedStream.value.channel.name, true),
-                type: cachedStream.value.stream_type
+                username: escapeDiscordMarkdown(stream.value.channel.display_name || stream.value.channel.name, true),
+                type: stream.value.stream_type
             }),
-            timestamp: cachedStream.value.created_at,
+            timestamp: stream.value.created_at,
             thumbnail: {
-                url: cachedStream.value.channel.logo,
+                url: stream.value.channel.logo,
                 width: 128,
                 height: 128
             },
             author: {
-                icon_url: cachedStream.value.channel.logo,
-                name: cachedStream.value.channel.display_name || cachedStream.value.channel.name,
-                url: cachedStream.value.channel.url
+                icon_url: stream.value.channel.logo,
+                name: stream.value.channel.display_name || stream.value.channel.name,
+                url: stream.value.channel.url
             },
-            title: cachedStream.value.channel.status,
+            title: stream.value.channel.status,
             color: TWITCH_COLOR,
             image: {
-                url: cachedStream.value.preview.template.replace("{width}", "1280").replace("{height}", "720") + `?ts=${Date.now()}`
+                url: stream.value.preview.template.replace("{width}", "1280").replace("{height}", "720") + `?ts=${Date.now()}`
             },
             fields: [{
                 inline: true,
                 name: localizer.getString(lang, "STREAMING_GAME_NAME"),
-                value: cachedStream.value.game ? cachedStream.value.game : localizer.getString(lang, "STREAMING_GAME_VALUE_UNKNOWN")
+                value: stream.value.game ? stream.value.game : localizer.getString(lang, "STREAMING_GAME_VALUE_UNKNOWN")
             }, {
                 inline: true,
                 name: localizer.getString(lang, "STREAMING_MATURE_NAME"),
                 value: localizer.getFormattedString(lang, "STREAMING_MATURE_VALUE_TWITCH", {
-                    mature: cachedStream.value.channel.mature
+                    mature: stream.value.channel.mature
                 })
             }]
         };
     }
 
-    private getAPIURL_Streams(ids:string[]) {
+    private getAPIURL_Streams(ids: string[]) {
         return `https://api.twitch.tv/kraken/streams/?channel=${ids.join(",")}&client_id=${this.clientId}&api_version=5`;
     }
 
-    // private getAPIURL_Channel(id:string) {
-    //     return `https://api.twitch.tv/kraken/streams/?channel=34711476&client_id=${this.clientId}1&api_version=5}`;
-    // }
-
-    private getAPIURL_User(username:string|string[]) {
+    private getAPIURL_User(username: string | string[]) {
         let uidsStr = username instanceof Array ? username.join(",") : username;
         return `https://api.twitch.tv/kraken/users?login=${uidsStr}&client_id=${this.clientId}&api_version=5`;
     }
 
-    public freed(uid:string) {
-        this.streamsCache.delete(uid);
+    public freed(uid: string) {
+        this.streamsMap.delete(uid);
     }
 
-    public async getStreamer(username:string, attempt=0) : Promise<IStreamingServiceStreamer> {
-        if(attempt > 3) {
-            throw new StreamingServiceError("TWITCH_TOOMANYATTEMPTS", "Too many attempts. Please, try again later.");
-        }
-
+    public async getStreamer(username: string): Promise<IStreamingServiceStreamer> {
         if(!TWITCH_USERNAME_REGEXP.test(username)) {
             throw new StreamingServiceError("TWITCH_INVALIDUSERNAME", "Invalid username.");
         }
 
-        let resp = await fetch(this.getAPIURL_User(username));
-        if(resp.status === 429) {
-            let delay = parseInt(resp.headers.get("retry-after") || "5000", 10);
-            this.log("info", `Ratelimited: waiting ${delay / 1000}sec.`);
-            await sleep(delay);
-            return await this.getStreamer(username, attempt++);
-        }
-
-        // it returns us an array with found usernames
-        let foundUsers = ((await resp.json()) as {
+        let foundUsers = (await this.makeRequest(this.getAPIURL_User(username)) as {
             users: ITwitchUser[]
         }).users;
 
         if(foundUsers.length === 0) {
-            // not found
             throw new StreamingServiceError("TWITCH_USERNOTFOUND", "User not found.");
         } else if(foundUsers.length > 1) {
-            // what the heck... ?
-            // whatever
             throw new StreamingServiceError("TWITCH_INVALIDRESPONSE", "Invalid response received.");
         }
 
@@ -189,36 +153,53 @@ class TwitchStreamingService implements IStreamingService {
         };
     }
 
+    private async makeRequest(uri: string): Promise<any> {
+        let loop = async (attempt: number = 0) => {
+            if(attempt > 3) {
+                throw new StreamingServiceError("TWITCH_TOOMANYATTEMPTS", "Too many attempts. Please, try again later.");
+            }
+            let resp = await fetch(uri);
+            if(resp.status === 429) {
+                let delay = parseInt(resp.headers.get("retry-after") || "5000", 10);
+                this.log("info", `Ratelimited: waiting ${delay / 1000}sec.`);
+                await sleep(delay);
+                return await loop(attempt + 1);
+            }
+            return await resp.json();
+        };
+        return await loop();
+    }
+
     async unload() {
-        this.streamsCache.clear();
+        this.streamsMap.clear();
         return true;
     }
 }
 
 interface ITwitchUser {
     /** Username with saved cAsE (to display) */
-    display_name:string;
+    display_name: string;
     /** ID */
-    _id:string;
+    _id: string;
     /** Username */
-    name:string;
+    name: string;
 }
 
 interface ITwitchChannel extends ITwitchUser {
     /** Mature? */
-    mature:boolean;
+    mature: boolean;
     /** Game name */
-    game:string;
+    game: string;
     /** Language */
-    language?:string;
+    language?: string;
     /** Channel owner avy */
-    logo:string;
+    logo: string;
     /** Url to stream */
-    url:string;
+    url: string;
     /** Current name of stream */
-    status:string;
+    status: string;
     /** Broadcaster language */
-    broadcaster_language?:string;
+    broadcaster_language?: string;
 }
 
 interface ITwitchStream {
@@ -241,16 +222,16 @@ interface ITwitchStream {
     /** It's vodcast? */
     "is_playlist": boolean;
     /** Type of stream */
-    "stream_type": "live"|"playlist"|"all";
+    "stream_type": "live" | "playlist" | "all";
     /** Previews */
-	"preview": {
-		"small": string;
-		"medium": string;
-		"large": string;
-		"template": string;
+    "preview": {
+        "small": string;
+        "medium": string;
+        "large": string;
+        "template": string;
     };
     /** Channel, lol */
-	"channel": ITwitchChannel;
+    "channel": ITwitchChannel;
 }
 
 module.exports = TwitchStreamingService;
