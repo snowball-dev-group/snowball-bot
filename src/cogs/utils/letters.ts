43343fbb6c6c8e5f17d206ac21bd31de895d88f7
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-08-13 02:24:22 +0700

    utils:letters: convert numbers upgraded a bit

--- src/cogs/utils/letters.ts
+++ src/cogs/utils/letters.ts
@@ -1,17 +1,6 @@
-const NUMBERS = [
-  "1⃣ ",
-  "2⃣ ",
-  "3⃣ ",
-  "4⃣ ",
-  "5⃣ ",
-  "6⃣ ",
-  "7⃣ ",
-  "8⃣ ",
-  "9⃣ ",
-  "0⃣"
-].map(n => n.trim());
-
 const REGIONAL_CHAR = String.fromCharCode(0xD83C);
+const KEYCAP_SUBCHAR = String.fromCharCode(0x20E3);
+
 const REGIONAL_SUBCHAR_START = 56806;
 
 export enum Stances {
@@ -48,18 +37,10 @@ export function toRegionalIndicators(str:string, stances?:Stances, unknownCharRe
 export function convertNumbers(number:string|number) {
     let str = number + "";
     str = str.replace(/([0-9])/ig, (s) => {
-        switch(s) {
-            case "1": return NUMBERS[0];
-            case "2": return NUMBERS[1];
-            case "3": return NUMBERS[2];
-            case "4": return NUMBERS[3];
-            case "5": return NUMBERS[4];
-            case "6": return NUMBERS[5];
-            case "7": return NUMBERS[6];
-            case "8": return NUMBERS[7];
-            case "9": return NUMBERS[8];
-            case "0": return NUMBERS[9];
-            default: return s;
+        if(/^[0-9]{1}$/.test(s)) {
+            return `${s}${KEYCAP_SUBCHAR}`;
+        } else {
+            return s;
         }
     });
     return str;
