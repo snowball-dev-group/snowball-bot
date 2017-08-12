e0d7b95bac34b3cf6ec3ae4a6209eedcfa730a07
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-08-13 02:27:21 +0700

    :: formatting in letters.ts

--- src/cogs/utils/letters.ts
+++ src/cogs/utils/letters.ts
@@ -8,7 +8,7 @@ export enum Stances {
     ConvertNumbers = 4
 }
 
-export function toRegionalIndicators(str:string, stances?:Stances, unknownCharReplacer?:(s:string) => string) : string {
+export function toRegionalIndicators(str: string, stances?: Stances, unknownCharReplacer?: (s: string) => string): string {
     let arr = str.split("");
     let allowCaseSwitching = false;
     let numbersConversion = true;
@@ -34,7 +34,7 @@ export function toRegionalIndicators(str:string, stances?:Stances, unknownCharRe
     return arr.join("");
 }
 
-export function convertNumbers(number:string|number, unknownCharReplacer?:(s:string) => string) {
+export function convertNumbers(number: string | number, unknownCharReplacer?: (s: string) => string) {
     let str = number + "";
     str = str.replace(/([0-9])/ig, (s) => {
         if(/^[0-9]{1}$/.test(s)) {
