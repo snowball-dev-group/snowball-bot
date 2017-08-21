983176329efa698e6b293f21287498e7eefddd87
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-08-22 03:19:16 +0700

    utils:random: more random functions

--- src/cogs/utils/random.ts
+++ src/cogs/utils/random.ts
@@ -1,3 +1,25 @@
-export function randomString(length: number) {
-    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+import * as Random from "random-js";
+
+export function getRandom(seed?:string) {
+    return new Random((() => {
+        let engine = Random.engines.mt19937();
+        
+        if(seed) {
+            engine.seedWithArray(seed.split("").map(c => c.charCodeAt(0)));
+        } else {
+            engine.autoSeed();
+        }
+
+        return engine;
+    })());
+}
+
+export function randomString(length: number, pool?:string, seed?:string) {
+    let random = getRandom(seed);
+    return random.string(length, pool);
+}
+
+export function randomPick<T>(array:T[], begin?:number, end?:number, seed?:string) : T {
+    let random = getRandom(seed);
+    return random.pick<T>(array, begin, end);
 }
