4939d76e74289c78eca922f767440e945da08d4b
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-09-10 05:45:54 +0700

    {Gruntfile, tsconfig.json, tslint.json}: oops

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,54 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        ts: {
-            default: {
-                tsconfig: true,
-                options: {
-                    fast: "never"
-                }
-            }
-        },
-        tslint: {
-            default: {
-                options: {
-                    configuration: "tslint.json",
-                    force: false,
-                    fix: false
-                },
-                files: {
-                    src: ["src/**/*.ts"]
-                }
-            }
-        },
-        copy: {
-            default: {
-                files: [{
-                    expand: true,
-                    src: ["src/languages/*.json"],
-                    dest: "out/languages/",
-                    flatten: true,
-                    filter: 'isFile'
-                }, {
-                    expand: true,
-                    src: ["src/run.sh", "src/run.bat"],
-                    dest: "out/",
-                    flatten: true,
-                    filter: "isFile"
-                }, {
-                    expand: true,
-                    src: ["package.json"],
-                    dest: "out/",
-                    flatten: true,
-                    filter: 'isFile'
-                }]
-            }
-        }
-    });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		ts: {
+			default: {
+				tsconfig: true,
+				options: {
+					fast: "never"
+				}
+			}
+		},
+		tslint: {
+			default: {
+				options: {
+					configuration: "tslint.json",
+					force: false,
+					fix: false
+				},
+				files: {
+					src: ["src/**/*.ts"]
+				}
+			}
+		},
+		copy: {
+			default: {
+				files: [{
+					expand: true,
+					src: ["src/languages/*.json"],
+					dest: "out/languages/",
+					flatten: true,
+					filter: 'isFile'
+				}, {
+					expand: true,
+					src: ["src/run.sh", "src/run.bat"],
+					dest: "out/",
+					flatten: true,
+					filter: "isFile"
+				}, {
+					expand: true,
+					src: ["package.json"],
+					dest: "out/",
+					flatten: true,
+					filter: 'isFile'
+				}]
+			}
+		}
+	});
 
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks("grunt-tslint");
-    grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks("grunt-ts");
+	grunt.loadNpmTasks("grunt-tslint");
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask("default", ["tslint", "ts", "copy"]);
+	grunt.registerTask("default", ["tslint", "ts", "copy"]);
 };
