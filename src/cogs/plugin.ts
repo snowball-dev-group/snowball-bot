908c6a9f2dfe42b1a0b96cfb545e6727de2c5a68
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-09-22 16:47:02 +0700

    Plugin: some changos

--- src/cogs/plugin.ts
+++ src/cogs/plugin.ts
@@ -1,170 +1,170 @@
 import { Message, Channel, Emoji, Guild, User, GuildMember, Snowflake, Collection, MessageReaction, Role } from "discord.js";
 
-export interface IEventsMap {
+export interface IEventsMap <T> {
 	/**
 	 * Emitted whenever a channel is created.
 	 */
-	channelCreate?: ((channel:Channel) => any)|Array<((channel:Channel) => any)>;
+	channelCreate?: ((channel:Channel) => T)|Array<((channel:Channel) => T)>;
 	/**
 	 * Emitted whenever a channel is deleted.
 	 */
-	channelDelete?: ((channel:Channel) => any)|Array<((channel:Channel) => any)>;
+	channelDelete?: ((channel:Channel) => T)|Array<((channel:Channel) => T)>;
 	/**
 	 * Emitted whenever the pins of a channel are updated. Due to the nature of the WebSocket event, not much information can be provided easily here - you need to manually check the pins yourself.
 	 */
-	channelPinsUpdate?: ((channel:Channel, time:Date) => any)|Array<((channel:Channel, time:Date) => any)>;
+	channelPinsUpdate?: ((channel:Channel, time:Date) => T)|Array<((channel:Channel, time:Date) => T)>;
 	/**
 	 * Emitted whenever a channel is updated - e.g. name change, topic change
 	 */
-	channelUpdate?: ((oldChannel:Channel, newChannel:Channel) => any)|Array<((oldChannel:Channel, newChannel:Channel) => any)>;
+	channelUpdate?: ((oldChannel:Channel, newChannel:Channel) => T)|Array<((oldChannel:Channel, newChannel:Channel) => T)>;
 	/**
 	 * Emitted when the client's WebSocket disconnects and will no longer attempt to reconnect.
 	 */
-	disconnect?: ((closeEvent) => any)|Array<((closeEvent) => any)>;
+	disconnect?: ((closeEvent) => T)|Array<((closeEvent) => T)>;
 	/**
 	 * Emitted whenever a custom emoji is created in a guild.
 	 * 
 	 */
-	emojiCreate?: ((emoji:Emoji) => any)|Array<((emoji:Emoji) => any)>;
+	emojiCreate?: ((emoji:Emoji) => T)|Array<((emoji:Emoji) => T)>;
 	/**
 	 * Emitted whenever a custom guild emoji is deleted.
 	 */
-	emojiDelete?: ((emoji:Emoji) => any)|Array<((emoji:Emoji) => any)>;
+	emojiDelete?: ((emoji:Emoji) => T)|Array<((emoji:Emoji) => T)>;
 	/**
 	 * Emitted whenever a custom guild emoji is updated.
 	 */
-	emojiUpdate?: ((oldEmoji:Emoji, newEmoji:Emoji) => any)|Array<((oldEmoji:Emoji, newEmoji:Emoji) => any)>;
+	emojiUpdate?: ((oldEmoji:Emoji, newEmoji:Emoji) => T)|Array<((oldEmoji:Emoji, newEmoji:Emoji) => T)>;
 	/**
 	 * Emitted whenever a member is banned from a guild.
 	 */
-	guildBanAdd?: ((guild:Guild, user:User) => any)|Array<((guild:Guild, user:User) => any)>;
+	guildBanAdd?: ((guild:Guild, user:User) => T)|Array<((guild:Guild, user:User) => T)>;
 	/**
 	 * Emitted whenever a member is unbanned from a guild.
 	 */
-	guildBanRemove?: ((guild:Guild, user:User) => any)|Array<((guild:Guild, user:User) => any)>;
+	guildBanRemove?: ((guild:Guild, user:User) => T)|Array<((guild:Guild, user:User) => T)>;
 	/**
 	 * Emitted whenever the client joins a guild.
 	 */
-	guildCreate?: ((guild:Guild) => any)|Array<((guild:Guild) => any)>;
+	guildCreate?: ((guild:Guild) => T)|Array<((guild:Guild) => T)>;
 	/**
 	 * Emitted whenever a guild is deleted/left.
 	 */
-	guildDelete?: ((guild:Guild) => any)|Array<((guild:Guild) => any)>;
+	guildDelete?: ((guild:Guild) => T)|Array<((guild:Guild) => T)>;
 	/**
 	 * Emitted whenever a user joins a guild.
 	 */
-	guildMemberAdd?: ((member:GuildMember) => any)|Array<((member:GuildMember) => any)>;
+	guildMemberAdd?: ((member:GuildMember) => T)|Array<((member:GuildMember) => T)>;
 	/**
 	 * Emitted whenever a member becomes available in a large guild.
 	 */
-	guildMemberAvailable?: ((member:GuildMember) => any)|Array<((member:GuildMember) => any)>;
+	guildMemberAvailable?: ((member:GuildMember) => T)|Array<((member:GuildMember) => T)>;
 	/**
 	 * Emitted whenever a member leaves a guild, or is kicked.
 	 */
-	guildMemberRemove?: ((member:GuildMember) => any)|Array<((member:GuildMember) => any)>;
+	guildMemberRemove?: ((member:GuildMember) => T)|Array<((member:GuildMember) => T)>;
 	/**
 	 * Emitted whenever a chunk of guild members is received (all members come from the same guild).
 	 */
-	guildMembersChunk?: ((members:Collection<Snowflake, GuildMember>, guild:Guild) => any)|Array<((members:Collection<Snowflake, GuildMember>, guild:Guild) => any)>;
+	guildMembersChunk?: ((members:Collection<Snowflake, GuildMember>, guild:Guild) => T)|Array<((members:Collection<Snowflake, GuildMember>, guild:Guild) => T)>;
 	/**
 	 * Emitted once a guild member starts/stops speaking.
 	 */
-	guildMemberSpeaking?: ((member:GuildMember, speaking:boolean) => any)|Array<((member:GuildMember, speaking:boolean) => any)>;
+	guildMemberSpeaking?: ((member:GuildMember, speaking:boolean) => T)|Array<((member:GuildMember, speaking:boolean) => T)>;
 	/**
 	 * Emitted whenever a guild member changes - i.e. new role, removed role, nickname.
 	 */
-	guildMemberUpdate?: ((oldMember:GuildMember, newMember:GuildMember) => any)|Array<((oldMember:GuildMember, newMember:GuildMember) => any)>;
+	guildMemberUpdate?: ((oldMember:GuildMember, newMember:GuildMember) => T)|Array<((oldMember:GuildMember, newMember:GuildMember) => T)>;
 	/**
 	 * Emitted whenever a guild becomes unavailable, likely due to a server outage.
 	 */
-	guildUnavailable?: ((guild:Guild) => any)|Array<((guild:Guild) => any)>;
+	guildUnavailable?: ((guild:Guild) => T)|Array<((guild:Guild) => T)>;
 	/**
 	 * Emitted whenever a guild is updated - e.g. name change.
 	 */
-	guildUpdate?: ((oldGuild:Guild, newGuild:Guild) => any)|Array<((oldGuild:Guild, newGuild:Guild) => any)>;
+	guildUpdate?: ((oldGuild:Guild, newGuild:Guild) => T)|Array<((oldGuild:Guild, newGuild:Guild) => T)>;
 	/**
 	 * Emitted whenever a message is created.
 	 */
-	message?: ((message:Message) => any)|Array<((message:Message) => any)>;
+	message?: ((message:Message) => T)|Array<((message:Message) => T)>;
 	/**
 	 * Emitted whenever a message is deleted.
 	 */
-	messageDelete?: ((message:Message) => any)|Array<((message:Message) => any)>;
+	messageDelete?: ((message:Message) => T)|Array<((message:Message) => T)>;
 	/**
 	 * Emitted whenever messages are deleted in bulk.
 	 */
-	messageDeleteBulk?: ((messages:Collection<Snowflake, Message>) => any)|Array<((messages:Collection<Snowflake, Message>) => any)>;
+	messageDeleteBulk?: ((messages:Collection<Snowflake, Message>) => T)|Array<((messages:Collection<Snowflake, Message>) => T)>;
 	/**
 	 * Emitted whenever a reaction is added to a message.
 	 */
-	messageReactionAdd?: ((messageReaction:MessageReaction, user:User) => any)|Array<((messageReaction:MessageReaction, user:User) => any)>;
+	messageReactionAdd?: ((messageReaction:MessageReaction, user:User) => T)|Array<((messageReaction:MessageReaction, user:User) => T)>;
 	/**
 	 * Emitted whenever a reaction is removed from a message.
 	 */
-	messageReactionRemove?: ((messageReaction:MessageReaction, user:User) => any)|Array<((messageReaction:MessageReaction, user:User) => any)>;
+	messageReactionRemove?: ((messageReaction:MessageReaction, user:User) => T)|Array<((messageReaction:MessageReaction, user:User) => T)>;
 	/**
 	 * Emitted whenever all reactions are removed from a message.
 	 */
-	messageReactionRemoveAll?: ((message:Message) => any)|Array<((message:Message) => any)>;
+	messageReactionRemoveAll?: ((message:Message) => T)|Array<((message:Message) => T)>;
 	/**
 	 * Emitted whenever a message is updated - e.g. embed or content change.
 	 */
-	messageUpdate?: ((oldMessage:Message, newMessage:Message) => any)|Array<((oldMessage:Message, newMessage:Message) => any)>;
+	messageUpdate?: ((oldMessage:Message, newMessage:Message) => T)|Array<((oldMessage:Message, newMessage:Message) => T)>;
 	/**
 	 * Emitted whenever a guild member's presence changes, or they change one of their details.
 	 */
-	presenceUpdate?: ((oldMember:GuildMember, newMember:GuildMember) => any)|Array<((oldMember:GuildMember, newMember:GuildMember) => any)>;
+	presenceUpdate?: ((oldMember:GuildMember, newMember:GuildMember) => T)|Array<((oldMember:GuildMember, newMember:GuildMember) => T)>;
 	/**
 	 * Emitted whenever the client tries to reconnect to the WebSocket.
 	 */
-	reconnecting?: (() => any)|Array<(() => any)>;
+	reconnecting?: (() => T)|Array<(() => T)>;
 	/**
 	 * Emitted whenever a WebSocket resumes.
 	 */
-	resume?: ((replayed:number) => any)|Array<((replayed:number) => any)>;
+	resume?: ((replayed:number) => T)|Array<((replayed:number) => T)>;
 	/**
 	 * Emitted whenever a role is created.
 	 */
-	roleCreate?: ((role:Role) => any)|Array<((role:Role) => any)>;
+	roleCreate?: ((role:Role) => T)|Array<((role:Role) => T)>;
 	/**
 	 * Emitted whenever a guild role is deleted.
 	 */
-	roleDelete?: ((role:Role) => any)|Array<((role:Role) => any)>;
+	roleDelete?: ((role:Role) => T)|Array<((role:Role) => T)>;
 	/**
 	 * Emitted whenever a guild role is updated.
 	 */
-	roleUpdate?: ((oldRole:Role, newRole:Role) => any)|Array<((oldRole:Role, newRole:Role) => any)>;
+	roleUpdate?: ((oldRole:Role, newRole:Role) => T)|Array<((oldRole:Role, newRole:Role) => T)>;
 	/**
 	 * Emitted whenever a user starts typing in a channel.
 	 */
-	typingStart?: ((channel:Channel, user:User) => any)|Array<((channel:Channel, user:User) => any)>;
+	typingStart?: ((channel:Channel, user:User) => T)|Array<((channel:Channel, user:User) => T)>;
 	/**
 	 * Emitted whenever a user stops typing in a channel.
 	 */
-	typingStop?: ((channel:Channel, user:User) => any)|Array<((channel:Channel, user:User) => any)>;
+	typingStop?: ((channel:Channel, user:User) => T)|Array<((channel:Channel, user:User) => T)>;
 	/**
 	 * Emitted whenever a user's details (e.g. username) are changed.
 	 */
-	userUpdate?: ((oldUser:User, newUser:User) => any)|Array<((oldUser:User, newUser:User) => any)>;
+	userUpdate?: ((oldUser:User, newUser:User) => T)|Array<((oldUser:User, newUser:User) => T)>;
 	/**
 	 * Emitted whenever a user changes voice state - e.g. joins/leaves a channel, mutes/unmutes.
 	 */
-	voiceStateUpdate?: ((oldMember:GuildMember, newMember:GuildMember) => any)|Array<((oldMember:GuildMember, newMember:GuildMember) => any)>;
+	voiceStateUpdate?: ((oldMember:GuildMember, newMember:GuildMember) => T)|Array<((oldMember:GuildMember, newMember:GuildMember) => T)>;
 }
 
 export class Plugin {
-	private eventsMap:IEventsMap;
+	private _eventsMap:IEventsMap<any>;
 
-	constructor(events: IEventsMap, dontAutoHandle = false) {
-		this.eventsMap = events;
+	constructor(events: IEventsMap<any>, dontAutoHandle = false) {
+		this._eventsMap = events;
 		if(!dontAutoHandle) {
 			this.handleEvents();
 		}
 	}
 
-	handleEvents() {
-		for(let key in this.eventsMap) {
-			let val = this.eventsMap[key];
+	public handleEvents() {
+		for(let key in this._eventsMap) {
+			let val = this._eventsMap[key];
 			if(Array.isArray(val)) {
 				for(let handler of val) {
 					$discordBot.on(key, handler);
@@ -176,8 +176,8 @@ export class Plugin {
 	}
 
 	unhandleEvents() {
-		for(let key in this.eventsMap) {
-			let val = this.eventsMap[key];
+		for(let key in this._eventsMap) {
+			let val = this._eventsMap[key];
 			if(Array.isArray(val)) {
 				for(let handler of val) {
 					$discordBot.removeListener(key, handler);
