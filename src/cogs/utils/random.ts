06b64e8777afd671b059d6c383162a85a3576719
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-12-15 01:15:15 +0700

    utils:random: added `randomNumber` function

--- src/cogs/utils/random.ts
+++ src/cogs/utils/random.ts
@@ -1,25 +1,28 @@
 import * as Random from "random-js";
 
-export function getRandom(seed?:string) {
+export function getRandom(seed?: string) {
 	return new Random((() => {
 		let engine = Random.engines.mt19937();
-		
+
 		if(seed) {
-			engine.seedWithArray(seed.split("").map(c => c.charCodeAt(0)));
-		} else {
-			engine.autoSeed();
-		}
+			engine = engine.seedWithArray(seed.split("").map(c => c.charCodeAt(0)));
+		} else { engine = engine.autoSeed(); }
 
 		return engine;
 	})());
 }
 
-export function randomString(length: number, pool?:string, seed?:string) {
-	let random = getRandom(seed);
+export function randomString(length: number, pool?: string, seed?: string) {
+	const random = getRandom(seed);
 	return random.string(length, pool);
 }
 
-export function randomPick<T>(array:T[], begin?:number, end?:number, seed?:string) : T {
-	let random = getRandom(seed);
+export function randomPick<T>(array: T[], begin?: number, end?: number, seed?: string): T {
+	const random = getRandom(seed);
 	return random.pick<T>(array, begin, end);
+}
+
+export function randomNumber(min: number, max: number, seed?: string) {
+	const random = getRandom(seed);
+	return random.integer(min, max);
 }
