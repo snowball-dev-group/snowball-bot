0d2f6a5d5c0c41ba9e74a9d9cb43a9e71c737a26
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-12-16 03:24:33 +0700

    ping: fixed wrong keeper was searched. dafri will learn someday how to copypaste

--- src/cogs/ping/ping.ts
+++ src/cogs/ping/ping.ts
@@ -22,7 +22,7 @@ class Ping extends Plugin implements IModule {
 	public async init() {
 		this.log("info", "Searching for `MessagesFlows` core keeper");
 
-		const messagesFlowsKeeper = $snowball.modLoader.loadedModulesRegistry["snowball.core_features.prefixall"];
+		const messagesFlowsKeeper = $snowball.modLoader.loadedModulesRegistry["snowball.core_features.messageflows"];
 
 		if(!messagesFlowsKeeper) {
 			throw new Error("`MessageFlows` not found");
