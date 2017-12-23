1323ca01f1f42e26c183c09fad9d7d02685fe1a8
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-12-23 13:38:19 +0700

    ping: many changes, more details

--- src/cogs/ping/ping.ts
+++ src/cogs/ping/ping.ts
@@ -5,6 +5,8 @@ import { command } from "../utils/help";
 import { getLogger } from "../utils/utils";
 import MessagesFlows, { IPublicFlowUnit, IMessageFlowContext } from "../cores/messagesFlows";
 
+const ALLOWED_CMDS = ["ping", "ping_embed"];
+
 @command("UTILITES", "ping", "loc:PING_CMDMETA_DEFAULT_DESCRIPTION")
 @command("UTILITES", "ping_embed", "loc:PING_CMDMETA_EMBED_DESCRIPTION")
 class Ping extends Plugin implements IModule {
@@ -29,9 +31,9 @@ class Ping extends Plugin implements IModule {
 		}
 
 		const initHandler = (flowsMan: MessagesFlows) => {
-			return flowsMan.watchForMessages((ctx) => this.onMessage(ctx), (ctx) => {
+			return this.flowHandler = flowsMan.watchForMessages((ctx) => this.onMessage(ctx), (ctx) => {
 				if(!ctx.parsed) { return false; }
-				return ctx.parsed.command ? ["ping", "ping_embed"].includes(ctx.parsed.command) : false;
+				return ctx.parsed.command ? ALLOWED_CMDS.includes(ctx.parsed.command) : false;
 			});
 		};
 
@@ -44,32 +46,23 @@ class Ping extends Plugin implements IModule {
 
 	async onMessage(ctx: IMessageFlowContext) {
 		if(!ctx.parsed) { return; }
-		let msg = ctx.message;
-		if(ctx.parsed.command === "ping" && !ctx.parsed.args) {
-			await msg.react("🏃");
-			let startDate = Date.now();
-			msg = await msg.channel.send(":information_source: Pong!") as Message;
-			let diff = Date.now() - startDate;
-			this.log("info", `Ping for sendMessage to Channel#${msg.channel.id}: ${diff}ms`);
-			msg.edit(`:information_source: Pong - \`${diff}ms\`!`);
-		} else if(ctx.parsed.command === "ping_embed" && !ctx.parsed.args) {
-			await msg.react("🏃");
-			let startDate = Date.now();
-			msg = await msg.channel.send("", {
-				embed: {
-					description: "Pong!"
-				}
-			}) as Message;
-			let diff = Date.now() - startDate;
-			this.log("info", `Ping for sendMessage#embed to Channel#${msg.channel.id}: ${diff}ms`);
-			msg.edit(``, {
-				embed: {
-					description: `:information_source: Pong - \`${diff}ms\`!`
-				}
-			});
-		} else {
-			this.log("warn_trace", "Called with unknown command!?");
-		}
+		if(!ALLOWED_CMDS.includes(ctx.parsed.command!)) { return; }
+
+		let pongStr = "ℹ Pong!";
+
+		const isEmbed = ctx.parsed.command === "ping_embed",
+		startDate = Date.now(),
+		msg = <Message>await ctx.message.channel.send(isEmbed ? { embed: { description: "Pong!" } } : "ℹ Pong!"),
+		receivedTime = Date.now(),
+		ping = Math.max(0, (msg.createdAt.getTime() - startDate)),
+		delay = (receivedTime - startDate) - ping, 
+		delayStr = delay >= 0 ? `+${delay}` : `${delay}`;
+
+		pongStr = `ℹ Pong - \`${ping}ms\` (\`${delayStr}ms\`)`;
+
+		this.log("info", `Ping for sendMessage#embed to Channel#${msg.channel.id}: ${ping}ms (${delayStr}ms)`);
+
+		return await msg.edit(isEmbed ? { embed: { description: pongStr } } : pongStr);
 	}
 
 	async unload() {
