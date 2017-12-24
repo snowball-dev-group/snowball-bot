424f78dd277646242afb523cc61d42b76d83b301
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2017-12-25 02:06:30 +0700

    utils:failToDetail: smoll changos pls trust me not gonna lie here

--- src/cogs/utils/failToDetail.ts
+++ src/cogs/utils/failToDetail.ts
@@ -1,13 +1,48 @@
-import { Message } from "discord.js";
+import { DMChannel, GroupDMChannel, Guild, GuildMember, Message, TextChannel, User } from "discord.js";
 
-export function messageToExtra(msg: Message, extra?: { [key: string]: any }) {
-    let generatedExtra =  {
-        guild: msg.guild,
-        channel: msg.channel,
-        user: msg.author,
+type Extra = { [key: string]: any };
+
+export function messageToExtra(msg: Message, extra?: Extra) {
+    return {
+		...extra,
+        guild: msg.guild ? guildToExtra(msg.guild) : undefined,
+        channel: channelToExtra(msg.channel),
+        user: userToExtra(msg.author),
         msgContent: msg.content,
         msgId: msg.id
     };
-    if(extra) { generatedExtra = { ...generatedExtra, ...extra }; }
-    return generatedExtra;
+}
+
+export function guildToExtra(guild: Guild, extra?: Extra) {
+	return {
+		...extra,
+		guildId: guild.id,
+		guildName: guild.name
+	};
+}
+
+export function channelToExtra(channel: TextChannel | DMChannel | GroupDMChannel, extra?: Extra) {
+	return {
+		...extra,
+		channelName: channel instanceof TextChannel ? channel.name : undefined,
+		channelId: channel.id,
+		channelType: channel.type
+	};
+}
+
+export function userToExtra(user: User, extra?: Extra) {
+	return {
+		...extra,
+		username: user.tag,
+		userId: user.id,
+		bot: user.bot
+	};
+}
+
+export function memberToExtra(guild: GuildMember, extra?: Extra) {
+	return {
+		...extra,
+		memberNick: guild.nickname,
+		memberJoinedAt: guild.joinedTimestamp
+	};
 }
