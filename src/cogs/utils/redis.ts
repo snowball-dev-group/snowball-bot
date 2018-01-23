9b0b49648f07819a2f0ffb475ebe851022918814
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-01-23 11:06:05 +0700

    utils:redis: basement for new caching util package{-lock}.json: added ioredis package

---
+++ src/cogs/utils/redis.ts
@@ -0,0 +1,47 @@
+import * as Redis from "ioredis";
+import { getLogger } from "./utils";
+
+let redisClient: undefined | Redis.Redis = undefined;
+
+const REDIS_DEFAULT_PORT = 6379;
+const REDIS_DEFAULT_HOST = "127.0.0.1";
+const LOG = getLogger("Utils:Redis");
+
+export async function getRedisClient() {
+	if(!redisClient) {
+		LOG("info", "[Get Client] Client not found, init cycle start...");
+		redisClient = await init();
+	}
+
+	return redisClient;
+}
+
+async function init() {
+	const portOverride = parseInt(process.env.REDIS_PORT!, 10);
+	const hostOverride = process.env.REDIS_HOST;
+	const passwordOverride = process.env.REDIS_PASSWD;
+
+	if(!portOverride) {
+		LOG("info", `[Init] Port override is not presented, using '${REDIS_DEFAULT_PORT}'`);
+	}
+	if(!hostOverride) {
+		LOG("info", `[Init] Host override is not presented, using '${REDIS_DEFAULT_HOST}'`);
+	}
+
+	redisClient = createRedisClient(isNaN(portOverride) ? REDIS_DEFAULT_PORT : portOverride, hostOverride ? hostOverride : REDIS_DEFAULT_HOST, {
+		password: passwordOverride
+	});
+
+	LOG("info", "[Init] Connecting...");
+	await redisClient.connect();
+
+	return redisClient;
+}
+
+export function createRedisClient(port?: number, host?: string, options?: Redis.RedisOptions) {
+	if(options && !options.password) {
+		LOG("warn", "[Client Creation] It's highly recommended to have password set.");
+	}
+
+	return new Redis(port || REDIS_DEFAULT_PORT, host || REDIS_DEFAULT_HOST, options);
+}
