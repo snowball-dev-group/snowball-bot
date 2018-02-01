a77f8321f8fd4ae68108b73190f27636e5b654df
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-02-02 04:16:38 +0700

    utils:semaphore: to regulate something utils:locker: to lock something

---
+++ src/cogs/utils/locker.ts
@@ -0,0 +1,133 @@
+import { INullableHashMap } from "../../types/Types";
+import { Guild, GuildChannel } from "discord.js";
+import { EventEmitter } from "events";
+import { isPromise } from "./extensions";
+
+type PossibleTargets = string | Guild | GuildChannel;
+type EmptyVoid = () => void;
+
+/**
+ * # Discord Locker
+ * 
+ * Creates special class to lock and unlock some target.
+ * 
+ * ## Features:
+ * 
+ * - Can lock and unlock of course! 👌
+ * - Calls any callback using process.nextTick (is this even a feature?)
+ * - Has lock function that returns Promise that resolves with result of onLock function
+ * - Has function to await for unlock and alias function to create Promise for this function
+ * 
+ * ## To note
+ * 
+ * Use semaphore if you could do changes at the same time or want to build queues.
+ */
+export default class DiscordLocker {
+	private _lockStates: INullableHashMap<boolean>;
+	private _dispatcher: EventEmitter;
+
+	/**
+	 * __Creates new locker__.
+	 * Use it where changing of some resource at the same time is impossible
+	 */
+	constructor() {
+		this._lockStates = Object.create(null);
+		this._dispatcher = new EventEmitter();
+	}
+
+	/**
+	 * Locks resource for selected target
+	 * @param targetId Target of where resource lock happens
+	 * @param onLock Callback function if lock happened
+	 * @param lockFailed Callback function if lock failed
+	 * @returns `true` if lock happened, otherwise `false`
+	 */
+	public lock(targetId: PossibleTargets, onLock: (unlock: EmptyVoid) => void, lockFailed?: EmptyVoid) {
+		if(typeof targetId !== "string") { targetId = this._normalizeTarget(targetId); }
+		if(this._lockStates[targetId]) {
+			lockFailed && process.nextTick(lockFailed);
+			return false;
+		}
+		this._lockStates[targetId] = true;
+		process.nextTick(() => onLock(this._createSingletimeUnlocker(<string> targetId)));
+		return true;
+	}
+
+	/**
+	 * __Locks resource and returns promise__, which resolves once onLock function is executed
+	 * @param targetId Target of where resource lock happens
+	 * @param onLock Callback function if lock happened
+	 * @param lockFailed Callback function if lock failed
+	 * @returns Promise, which resolves with result of onLock function once unlock happens and rejects if lock failed
+	 */
+	public lockAwait<T = void>(targetId: PossibleTargets, onLock: (unlock: EmptyVoid) => T, lockFailed?: EmptyVoid) : Promise<T> {
+		if(typeof targetId !== "string") { targetId = this._normalizeTarget(targetId); }
+		return new Promise((res, rej) => {
+			const isLocked = this.lock(targetId, async (unlock) => {
+				const onLockResult = onLock(unlock);
+				return isPromise<T>(onLockResult) ? res(await onLockResult) : res(onLockResult);
+			}, lockFailed);
+
+			if(!isLocked) {
+				// `lock` always returns `false` if already locked
+				return rej({
+					code: "LOCK_IN_EFFECT",
+					message: "The lock is already in effect"
+				});
+			}
+		});
+	}
+
+	/**
+	 * Waits till resource is unlocked and then calls special function (aka callback)
+	 * @param targetId Target of where lock happened
+	 * @param onUnlock Callback function if resource is not locked or was unlocked
+	 * @param args Arguments to pass to callback function
+	 */
+	public waitForUnlock<T>(targetId: PossibleTargets, onUnlock: (...args: T[]) => void, ...args: T[]) {
+		if(typeof targetId !== "string") { targetId = this._normalizeTarget(targetId); }
+		if(!this._lockStates[targetId]) { return process.nextTick(onUnlock, ...args); }
+		this._dispatcher.once(`${targetId}:unlock`, () => process.nextTick(onUnlock, ...args));
+	}
+
+	/**
+	 * __Creates Promise, which resolves when resource is unlocked__.
+	 * Use `waitForUnlock` instead if you don't need Promise
+	 * @param targetId Target of where lock happened
+	 * @param value Value to resolve Promise with
+	 * @returns Promise, which resolves when resource is unlocked
+	 */
+	public awaitForUnlock<T>(targetId: PossibleTargets, value?: T) {
+		return new Promise((res) => this.waitForUnlock(targetId, res, value));
+	}
+
+	/**
+	 * Unlocks resource for selected target
+	 * @param targetId Target of where lock in effect
+	 * @returns `true` if unlock happened, otherwise `false`
+	 */
+	public unlock(targetId: PossibleTargets) {
+		if(typeof targetId !== "string") { targetId = this._normalizeTarget(targetId); }
+		if(!this._lockStates[targetId]) { return false; }
+		this._lockStates[targetId] = false;
+		this._dispatcher.emit(`${targetId}:unlock`);
+		return true;
+	}
+
+	private _createSingletimeUnlocker(targetId: string) {
+		let isUsed = false;
+		return () => {
+			if(isUsed) { throw new Error("This target is already unlocked"); }
+			return (isUsed = true) && this.unlock(targetId);
+		};
+	}
+
+	private _normalizeTarget(target: PossibleTargets) {
+		if(target instanceof Guild) {
+			return `g[${target.id}]`;
+		} else if(target instanceof GuildChannel) {
+			return `${target.type}[${target.id}]`;
+		}
+		return target;
+	}
+}
---
+++ src/cogs/utils/semaphore.ts
@@ -0,0 +1,143 @@
+import { INullableHashMap } from "../../types/Types";
+import { Guild, GuildChannel } from "discord.js";
+import { isPromise } from "./extensions";
+
+export const DEFAULT_CAPACITY = 1;
+
+type IQueueItem = (leave: () => number) => void;
+type PossibleTargets = string | Guild | GuildChannel;
+
+export default class DiscordSemaphore {
+	private _queue: INullableHashMap<IQueueItem[]> = Object.create(null);
+	private _currentWorkers: INullableHashMap<number> = Object.create(null);
+	private _capacity: number;
+
+	/**
+	 * Creates new semaphore for your 'track'
+	 * @param capacity Capacity of 'track' after which other task should wait in queue
+	 */
+	constructor(capacity = DEFAULT_CAPACITY) {
+		this._capacity = Math.max(1, capacity);
+	}
+
+	/**
+	 * Checks if 'track' for selected target is free
+	 * @param targetId Selected target
+	 * @returns `true` if number of active workers is equal to zero, otherwise false
+	 */
+	public isFree(targetId: PossibleTargets) {
+		if(typeof targetId !== "string") { targetId = this._normalizeTarget(targetId); }
+		return this._getWorkersCount(targetId) === 0;
+	}
+
+	/**
+	 * Checks number of remaining tasks in the queue of selected target
+	 * @param targetId Selected target
+	 * @returns Number of remaining tasks in the queue
+	 */
+	public queueSize(targetId: PossibleTargets) {
+		if(typeof targetId !== "string") { targetId = this._normalizeTarget(targetId); }
+		return this._getQueue(targetId).length;
+	}
+
+	/**
+	 * Pushes function to the queue
+	 * @param targetId Selected target
+	 * @param task Task to run
+	 */
+	public take(targetId: PossibleTargets, task: IQueueItem) {
+		if(typeof targetId !== "string") { targetId = this._normalizeTarget(targetId); }
+
+		const queue = this._getQueue(targetId);
+
+		queue.push(task);
+
+		process.nextTick(() => this._proceedQueue(<string> targetId));
+	}
+
+	/**
+	 * __Pushes special function to the queue__, which resolves promise once task is done.
+	 * This is preferable option if you want to get returned result of your task
+	 * @param targetId Selected target
+	 * @param task Task to run
+	 * @returns Promise which resolves with number of remaining workers and returned result. Rejects if task returned an error
+	 */
+	public takeAsync<T = void>(targetId: PossibleTargets, task: () => T | Promise<T>): Promise<[number, T]> {
+		return new Promise((res, rej) => {
+			this.take(targetId, async (leaver) => {
+				try {
+					let taskResult: any = task();
+					isPromise(taskResult) && (taskResult = await taskResult);
+					return res([leaver(), taskResult]);
+				} catch(err) {
+					return rej(err);
+				}
+			});
+		});
+	}
+
+	/**
+	 * __Decrease number of workers by one__.
+	 * Is is strongly unrecommended to use this function:
+	 * instead use single use leaver function in callback
+	 * @param targetId Discord Guild ID
+	 * @returns Number of remaining workers
+	 */
+	public leave(targetId: string) {
+		const workersRemaining = this._incrementWorkersCount(targetId, true); // decrement
+		process.nextTick(() => this._proceedQueue(targetId));
+		return workersRemaining; // useful data I guess
+	}
+
+	private _normalizeTarget(target: Guild | GuildChannel) {
+		if(target instanceof Guild) { return `g[${target.id}]`; }
+		return `${target.type}[${target.id}]`;
+	}
+
+	private _getQueue(targetId: string) {
+		return this._queue[targetId] || (this._queue[targetId] = []);
+	}
+
+	private _getWorkersCount(targetId: string) {
+		const workersCount = this._currentWorkers[targetId];
+		if(workersCount == null) { return this._currentWorkers[targetId] = 0; }
+		return workersCount;
+	}
+
+	private _incrementWorkersCount(targetId: string, invert = false) {
+		let currentWorkers = this._currentWorkers[targetId] || 0;
+		if(invert) {
+			if(currentWorkers === 0) { return currentWorkers; } // can go lower than 0
+			return this._currentWorkers[targetId] = currentWorkers--;
+		}
+		return this._currentWorkers[targetId] = currentWorkers++;
+	}
+
+	private _createSingleuseLeaver(targetId: string) {
+		let isUsed = false;
+		return () => {
+			// anti-smartness: storing usage and throwing an error if used
+			if(isUsed) { throw new Error("You already marked that this worker stopped working"); }
+			return (isUsed = true) && this.leave(targetId);
+		};
+	}
+
+	private _proceedQueue(targetId: string) {
+		const activeWorkers = this._getWorkersCount(targetId);
+
+		// are we working or overworking?
+		if(activeWorkers >= this._capacity) { return; }
+
+		const queue = this._getQueue(targetId);
+
+		const runner = queue.shift();
+
+		// do we need to run it?
+		if(typeof runner !== "function") { return; }
+
+		this._incrementWorkersCount(targetId);
+
+		// kinda async?
+		process.nextTick(() => runner(this._createSingleuseLeaver(targetId)));
+	}
+}
