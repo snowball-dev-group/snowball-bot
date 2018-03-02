ed17b58101f76ffe6c4a6a854b7f4d366ed4b293
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-03-03 04:39:34 +0700

    setLanguage: add timezones support + google timezone resolving
    This is not done yet, but available to test. Use with caution.

--- src/cogs/setLanguage/setLanguage.ts
+++ src/cogs/setLanguage/setLanguage.ts
@@ -1,29 +1,39 @@
 import { IModule } from "../../types/ModuleLoader";
 import { Plugin } from "../plugin";
-import { Message, GuildMember } from "discord.js";
+import { Message, GuildMember, User } from "discord.js";
 import { categoryLocalizedName, command } from "../utils/help";
-import { localizeForUser, getPrefsNames, forceUserLanguageUpdate, forceGuildEnforceUpdate, forceGuildLanguageUpdate, generateLocalizedEmbed, getUserLanguage } from "../utils/ez-i18n";
+import { localizeForUser, getPreferencesNames, forceUserLanguageUpdate, forceGuildEnforceUpdate, forceGuildLanguageUpdate, generateLocalizedEmbed, getUserLanguage, getUserTimezone, getGuildTimezone } from "../utils/ez-i18n";
 import { startsWith } from "../utils/text";
-import { EmbedType } from "../utils/utils";
+import { EmbedType, getMessageMemberOrAuthor, getMessageMember } from "../utils/utils";
 import { setPreferenceValue as setUserPref } from "../utils/userPrefs";
 import { setPreferenceValue as setGuildPref, getPreferenceValue as getGuildPref } from "../utils/guildPrefs";
 import { IHashMap, createHashMap } from "../../types/Types";
-import * as getLogger from "loggy";
 import { messageToExtra } from "../utils/failToDetail";
+import { DateTime } from "luxon";
+import { intlAcceptsTimezone } from "../utils/extensions";
+import { default as fetch } from "node-fetch";
+import * as getLogger from "loggy";
+import { URL } from "url";
+import { get, storeValue } from "../utils/cache";
+import { createConfirmationMessage } from "../utils/interactive";
 
 const BASE_PREFIX = "!sb_lang";
 const CMD = {
 	SWITCH: `${BASE_PREFIX} switch`,
+	TIMEZONE: `${BASE_PREFIX} timezone`,
 	CODES: `${BASE_PREFIX} codes`,
 	GUILDS_SWITCH: `${BASE_PREFIX} guild switch`,
+	GUILDS_TIMEZONE: `${BASE_PREFIX} guilds timezone`,
 	GUILDS_ENFORCE: `${BASE_PREFIX} guild enforce`
 };
 const HELP_CATEGORY = "LANGUAGE";
+const CACHE_OWNER = "snowball.setlanguage.locations";
 
 interface ISetLanguageCommandOptions {
 	no_lazy: boolean;
 	crowdinLink: string;
 	flags: IHashMap<string>;
+	googleApiKey?: string;
 }
 
 @command(HELP_CATEGORY, BASE_PREFIX.slice(1), "loc:LANGUAGE_META_DEFAULT")
@@ -57,6 +67,7 @@ class SetLanguageCommand extends Plugin implements IModule {
 	private readonly flags: IHashMap<string> = Object.create(null);
 	private readonly noLazy: boolean;
 	private readonly crowdinLink: string;
+	private readonly googleMapsApiKey?: string;
 
 	constructor(options: ISetLanguageCommandOptions) {
 		super({
@@ -65,6 +76,7 @@ class SetLanguageCommand extends Plugin implements IModule {
 		if(options) {
 			this.noLazy = !!options["no_lazy"];
 			this.crowdinLink = options.crowdinLink;
+			this.googleMapsApiKey = options.googleApiKey;
 			this.flags = createHashMap(options.flags);
 		} else { throw new Error("No options found"); }
 	}
@@ -101,8 +113,12 @@ class SetLanguageCommand extends Plugin implements IModule {
 				return await this.getCurrentLang(msg);
 			} else if(startsWith(msg.content, CMD.SWITCH)) {
 				return await this.switchLanguage(msg);
+			} else if(startsWith(msg.content, CMD.TIMEZONE)) {
+				return await this.timezone(msg);
 			} else if(startsWith(msg.content, CMD.GUILDS_SWITCH)) {
 				return await this.guildSwitch(msg);
+			} else if(startsWith(msg.content, CMD.GUILDS_TIMEZONE)) {
+				return await this.guildTimezone(msg);
 			} else if(startsWith(msg.content, CMD.GUILDS_ENFORCE)) {
 				return await this.guildEnforce(msg);
 			} else if(startsWith(msg.content, CMD.CODES)) {
@@ -310,6 +326,178 @@ class SetLanguageCommand extends Plugin implements IModule {
 		});
 	}
 
+	// #region Timezones Stuff
+
+	private async timezone(msg: Message) {
+		const msgAuthor = await getMessageMemberOrAuthor(msg);
+		if(!msgAuthor) { return; }
+
+		const userTimezone = await getUserTimezone(msgAuthor);
+
+		if(msg.content === CMD.TIMEZONE) {
+			return msg.channel.send({
+				embed: await generateLocalizedEmbed(EmbedType.Information, msgAuthor, {
+					custom: true,
+					string: await SetLanguageCommand._timezoneCurrentTime(
+						await localizeForUser(msgAuthor, "LANGUAGE_TIMEZONE_CURRENT@GUILD", {
+							timezone: userTimezone
+						}
+					), msgAuthor, userTimezone)
+				})
+			});
+		}
+
+		let newTZ = msg.content.slice(CMD.TIMEZONE.length);
+
+		if(!intlAcceptsTimezone(newTZ)) {
+			const result = await SetLanguageCommand._timezoneFallback(msg, msgAuthor, newTZ, this.googleMapsApiKey);
+			if(!result) { return; }
+			newTZ = result;
+		}
+
+		await setUserPref(msgAuthor, this.prefs.userTimezone, newTZ);
+
+		return msg.channel.send({
+			embed: await generateLocalizedEmbed(EmbedType.Information, msgAuthor, {
+				custom: true,
+				string: await SetLanguageCommand._timezoneCurrentTime(
+					await localizeForUser(msgAuthor, "LANGUAGE_TIMEZONE_DONE@USER", {
+						newTimzone: newTZ
+					}
+				), msgAuthor, newTZ)
+			})
+		});
+	}
+
+	private async guildTimezone(msg: Message) {
+		const msgMember = await getMessageMember(msg);
+		if(!msgMember) { return; }
+
+		const guildTimezone = await getGuildTimezone(msgMember.guild);
+
+		if(msg.content === CMD.TIMEZONE) {
+			return msg.channel.send({
+				embed: await generateLocalizedEmbed(EmbedType.Information, msgMember, {
+					custom: true,
+					string: await SetLanguageCommand._timezoneCurrentTime(
+						await localizeForUser(msgMember, "LANGUAGE_TIMEZONE_CURRENT@GUILD", {
+							timezone: guildTimezone
+						}
+					), msgMember, guildTimezone)
+				})
+			});
+		}
+
+		let newTZ = msg.content.slice(CMD.TIMEZONE.length);
+
+		if(!intlAcceptsTimezone(newTZ)) {
+			const result = await SetLanguageCommand._timezoneFallback(msg, msgMember, newTZ, this.googleMapsApiKey);
+			if(!result) { return; }
+			newTZ = result;
+		}
+
+		await setGuildPref(msgMember.guild, this.prefs.userTimezone, newTZ);
+
+		return msg.channel.send({
+			embed: await generateLocalizedEmbed(EmbedType.Information, msgMember, {
+				custom: true,
+				string: await SetLanguageCommand._timezoneCurrentTime(
+					await localizeForUser(msgMember, "LANGUAGE_TIMEZONE_DONE@GUILD", {
+						newTimzone: newTZ
+					}
+				), msgMember, newTZ)
+			})
+		});
+	}
+
+	private static async _timezoneFallback(msg: Message, msgAuthor: GuildMember | User, newTZ: string, apiKey?: string) : Promise<string|undefined> {
+		if(!apiKey) {
+			await msg.channel.send({
+				embed: await generateLocalizedEmbed(EmbedType.Error, msgAuthor, "LANGUAGE_TIMEZONE_INVALID")
+			});
+			return;
+		}
+
+		// do we have cache for this?
+
+		const cached = await get<string>(CACHE_OWNER, newTZ.toLowerCase());
+
+		if(cached == null) {
+			// resolving address with google maps
+
+			const findResp = await SetLanguageCommand._sendGoogleMapsReq<IGoogleGeocodingResponse>("geocode", apiKey, {
+				address: newTZ,
+				language: await localizeForUser(msgAuthor, "+SHORT_CODE")
+			});
+
+			if(findResp.status !== "OK" || findResp.results.length === 0) {
+				await msg.channel.send({
+					embed: await generateLocalizedEmbed(EmbedType.Error, msgAuthor, "LANGUAGE_TIMEZONE_NOT_FOUND")
+				});
+				return;
+			}
+
+			const selectedAddress = findResp.results[0]; // first one
+
+			const confirmationEmbed = await generateLocalizedEmbed(EmbedType.Question, msgAuthor, {
+				key: "LANGUAGE_TIMEZONE_CONFIRMATION@FOUND_ADDRESS",
+				formatOptions: { address: selectedAddress.formatted_address }
+			});
+
+			const confirmation = await createConfirmationMessage(confirmationEmbed, msg);
+
+			if(!confirmation) {
+				await msg.channel.send({
+					embed: await generateLocalizedEmbed(EmbedType.Error, msgAuthor, "LANGUAGE_TIMEZONE_CANCELED@FOUND_ADDRESS")
+				});
+				return;
+			}
+
+			// trying to resolve timezone
+
+			const timezoneResp = await SetLanguageCommand._sendGoogleMapsReq<IGoogleTimezoneResponse>("timezone", apiKey, {
+				timestamp: (Date.now() / 1000).toFixed(0),
+				location: `${selectedAddress.geometry.location.lat},${selectedAddress.geometry.location.lng}`
+			});
+
+			if(timezoneResp.status !== "OK" || intlAcceptsTimezone(timezoneResp.timeZoneId)) {
+				await msg.channel.send({
+					embed: await generateLocalizedEmbed(EmbedType.Error, msgAuthor, "LANGUAGE_TIMEZONE_APIERR@TIMEZONE_RESOLVE", {
+						universalTitle: await localizeForUser(msgAuthor, "LANGUAGE_TIMEZONE_CANCELED_TITLE")
+					})
+				});
+				return;
+			}
+
+			storeValue(CACHE_OWNER, newTZ, timezoneResp.timeZoneId, 604800);
+
+			newTZ = timezoneResp.timeZoneId;
+		} else { newTZ = cached; }
+	}
+
+	private static async _sendGoogleMapsReq<T>(apiType: string, apiKey: string, query: { [key: string]: string }) : Promise<T> {
+		const uri = new URL(`https://maps.googleapis.com/maps/api/${apiType}/json`);
+		
+		for(const key in query) { uri.searchParams.set(key, query[key]); }
+
+		uri.searchParams.set("key", apiKey);
+
+		const resp = await fetch(uri.toString());
+
+		if(resp.status !== 200) { throw new Error(`Invalid response code`); }
+
+		return resp.json();
+	}
+
+	private static async _timezoneCurrentTime(str: string, msgAuthor: GuildMember | User, timezone: string) {
+		str += `\n\n${await localizeForUser(msgAuthor, "LANGUAGE_TIMEZONE_TIME", {
+			currentTime: DateTime.local().setZone(timezone).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
+		})}`;
+		return str;
+	}
+
+	// #endregion
+
 	private static _isAdmin(member: GuildMember) {
 		return member.permissions.has(["ADMINISTRATOR", "MANAGE_GUILD", "MANAGE_CHANNELS", "MANAGE_ROLES"], true);
 	}
@@ -322,5 +510,27 @@ class SetLanguageCommand extends Plugin implements IModule {
 		return true;
 	}
 }
+
+interface IGoogleGeocodingResponse extends IGoogleResponse {
+	results: Array<{
+		formatted_address: string;
+		geometry: {
+			location: {
+				lat: number;
+				lng: number;
+			}
+		}
+	}>;
+}
+
+interface IGoogleTimezoneResponse extends IGoogleResponse {
+	timeZoneId: string;
+	timeZoneName: string;
+}
+
+interface IGoogleResponse {
+	status: string;
+}
+
 
 module.exports = SetLanguageCommand;
