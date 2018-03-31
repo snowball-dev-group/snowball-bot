52a217bb4eac71f1461e1b01ef47137de3e71fcd
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-04-01 03:53:26 +0700

    ownerCmds: code adaptation to recent changes

--- src/cogs/ownerCmds/ownerCmds.ts
+++ src/cogs/ownerCmds/ownerCmds.ts
@@ -1,10 +1,11 @@
 import { IModule } from "../../types/ModuleLoader";
-import logger = require("loggy");
 import { Plugin } from "../plugin";
 import { Message } from "discord.js";
 import { generateLocalizedEmbed } from "../utils/ez-i18n";
-import { commandRedirect, objectToMap, EmbedType, escapeDiscordMarkdown } from "../utils/utils";
+import { EmbedType, escapeDiscordMarkdown, getMessageMemberOrAuthor } from "../utils/utils";
 import { default as fetch } from "node-fetch";
+import { parse, commandRedirect } from "../utils/command";
+import * as logger from "loggy";
 
 class OwnerCommands extends Plugin implements IModule {
 	public get signature() {
@@ -22,73 +23,91 @@ class OwnerCommands extends Plugin implements IModule {
 	async onMessage(msg: Message) {
 		if (!msg.author) { return; }
 		if (msg.author.id !== $botConfig.botOwner) { return; }
-		const u = msg.member || msg.author;
-		commandRedirect(msg.content, objectToMap<Function>({
-			"!change_name": async (username) => {
+
+		const caller = await getMessageMemberOrAuthor(msg);
+
+		if (!caller) { return; }
+
+		const parsed = parse(msg.content);
+
+		return commandRedirect(parsed, {
+			"!change_name": async () => {
 				try {
 					const oldName = $discordBot.user.username;
-					const newUser = await $discordBot.user.setUsername(username);
+					const newUser = await $discordBot.user.setUsername(
+						parsed.content
+					);
 					msg.react("✅");
-					msg.channel.send("", {
-						embed: await generateLocalizedEmbed(EmbedType.OK, u, {
-							key: "OWNERCMDS_CHANGENAME_DONE",
-							formatOptions: {
-								oldName: escapeDiscordMarkdown(oldName, true),
-								newName: escapeDiscordMarkdown(newUser.username, true)
-							}
-						})
+					return msg.channel.send("", {
+						embed: await generateLocalizedEmbed(
+							EmbedType.OK, caller, {
+								key: "OWNERCMDS_CHANGENAME_DONE",
+								formatOptions: {
+									oldName: escapeDiscordMarkdown(oldName, true),
+									newName: escapeDiscordMarkdown(newUser.username, true)
+								}
+							})
 					});
 				} catch (err) {
 					msg.react("🚫");
-					msg.channel.send("", {
-						embed: await generateLocalizedEmbed(EmbedType.Error, u, {
-							key: "OWNERCMDS_CHANGENAME_FAULT",
-							formatOptions: {
-								errMessage: err.message
-							}
-						})
+					return msg.channel.send("", {
+						embed: await generateLocalizedEmbed(
+							EmbedType.Error, caller, {
+								key: "OWNERCMDS_CHANGENAME_FAULT",
+								formatOptions: {
+									errMessage: err.message
+								}
+							})
 					});
 				}
 			},
 			"!change_avy": async () => {
 				try {
 					const resp = await fetch(msg.attachments.first().url);
+
 					if (resp.status !== 200) {
-						msg.channel.send("", {
-							embed: await generateLocalizedEmbed(EmbedType.Progress, u, "OWNERCMDS_CHANGEAVY_FAULT_RESPERR")
+						return msg.channel.send("", {
+							embed: await generateLocalizedEmbed(
+								EmbedType.Progress, caller,
+								"OWNERCMDS_CHANGEAVY_FAULT_RESPERR"
+							)
 						});
-						return;
 					}
+
 					try {
 						const newUser = await $discordBot.user.setAvatar(await resp.buffer());
-						msg.channel.send("", {
-							embed: await generateLocalizedEmbed(EmbedType.OK, u, "OWNERCMDS_CHANGEAVY_DONE", {
-								imageUrl: newUser.displayAvatarURL({ format: "png", size: 1024 })
-							})
+
+						return msg.channel.send("", {
+							embed: await generateLocalizedEmbed(
+								EmbedType.OK, caller, "OWNERCMDS_CHANGEAVY_DONE", {
+									imageUrl: newUser.displayAvatarURL({ format: "png", size: 1024 })
+								})
 						});
 					} catch (err) {
-						msg.channel.send("", {
-							embed: await generateLocalizedEmbed(EmbedType.Error, u, {
-								key: "OWNERCMDS_CHANGEAVY_FAULT_SETFAILED",
-								formatOptions: {
-									errMessage: err.message
-								}
-							})
+						return msg.channel.send("", {
+							embed: await generateLocalizedEmbed(
+								EmbedType.Error, caller, {
+									key: "OWNERCMDS_CHANGEAVY_FAULT_SETFAILED",
+									formatOptions: {
+										errMessage: err.message
+									}
+								})
 						});
 					}
 				} catch (err) {
 					this.log("err", "Error downloading avy");
-					msg.channel.send("", {
-						embed: await generateLocalizedEmbed(EmbedType.Error, u, {
-							key: "OWNERCMDS_CHANGEAVY_FAULT_REQERROR",
-							formatOptions: {
-								errMsg: err.message
-							}
-						})
+					return msg.channel.send("", {
+						embed: await generateLocalizedEmbed(
+							EmbedType.Error, caller, {
+								key: "OWNERCMDS_CHANGEAVY_FAULT_REQERROR",
+								formatOptions: {
+									errMsg: err.message
+								}
+							})
 					});
 				}
 			}
-		}));
+		});
 	}
 
 	async unload() {
