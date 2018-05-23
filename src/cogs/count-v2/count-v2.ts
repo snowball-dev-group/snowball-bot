70e7f4e311f5ffcd0b1074e2bedc415dce006643
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-05-24 05:52:20 +0700

    count-v2: make XPOperation const enum count-v2: fix (ensure) visibility of methods and props

--- src/cogs/count-v2/count-v2.ts
+++ src/cogs/count-v2/count-v2.ts
@@ -14,7 +14,7 @@ enum DBInitializationState {
 	FullyInitialized = NotInitialized | MainTableInitialized | ScoreboardInitialized
 }
 
-enum XPOperation {
+const enum XPOperation {
 	DoNotChange = 0,
 	Raise = 1,
 	Lower = 2
@@ -68,33 +68,33 @@ class CountV2 extends Plugin implements IModule {
 		return "dafri.interactive.count-v2";
 	}
 
-	log = getLogger("CountV2Channel");
-	dbClient: knex;
-	countRegex: RegExp;
-	dbInitialized: DBInitializationState = DBInitializationState.NotInitialized;
-	scoreboardMessages: {
+	private static readonly _countRegex = /^\d{0,}$/i;
+	private readonly _log = getLogger("CountV2Channel");
+	private readonly _db: knex;
+	private _dbInitialized: DBInitializationState = DBInitializationState.NotInitialized;
+	private readonly _scoreboardMessages: {
 		top10?: Message,
 		latestChanges?: Message
 	} = {
 		top10: undefined,
 		latestChanges: undefined
 	};
-	latestScoreboardUpdate = new Date();
+	// private _latestScoreboardUpdate = new Date();
 
 	constructor() {
 		super({
-			"message": (msg: Message) => this.onMessage(msg)
+			"message": (msg: Message) => this._onMessage(msg)
 		});
-		this.dbClient = getDB();
+		this._db = getDB();
 
-		this.dbClient.schema.hasTable(TABLENAME_MAIN).then(itHas => {
+		this._db.schema.hasTable(TABLENAME_MAIN).then(itHas => {
 			if (itHas) {
-				this.log("ok", `DB: we have table '${TABLENAME_MAIN}', can safely continue work...`);
-				this.dbInitialized = this.dbInitialized | DBInitializationState.MainTableInitialized;
+				this._log("ok", `DB: we have table '${TABLENAME_MAIN}', can safely continue work...`);
+				this._dbInitialized = this._dbInitialized | DBInitializationState.MainTableInitialized;
 				return;
 			}
-			this.log("warn", `DB: seems we doesn't have table '${TABLENAME_MAIN}' in database, going to create it right now`);
-			this.dbClient.schema.createTable(TABLENAME_MAIN, (tb) => {
+			this._log("warn", `DB: seems we doesn't have table '${TABLENAME_MAIN}' in database, going to create it right now`);
+			this._db.schema.createTable(TABLENAME_MAIN, (tb) => {
 				tb.integer("count").notNullable();
 				tb.string("author").notNullable();
 				tb.string("date").notNullable();
@@ -103,58 +103,56 @@ class CountV2 extends Plugin implements IModule {
 				tb.string("answered_by").notNullable();
 				tb.string("in_queue").notNullable().defaultTo("-1");
 			}).catch(err => {
-				this.log("err", "DB: we can't prepare DB", err);
+				this._log("err", "DB: we can't prepare DB", err);
 			}).then(() => {
-				this.log("ok", "DB: we successfully prepared our DB and checking for existing elements");
+				this._log("ok", "DB: we successfully prepared our DB and checking for existing elements");
 				// this.dbInitialized = this.dbInitialized | DBInitializationState.MainTableInitialized;
-				this.dbClient(TABLENAME_MAIN).first().then((elem) => {
+				this._db(TABLENAME_MAIN).first().then((elem) => {
 					if (!elem) {
-						this.firstTime();
+						this._firstTimeBoot();
 					}
 				});
 			});
 		});
 
-		this.dbClient.schema.hasTable(TABLENAME_SCOREBOARD).then(itHas => {
+		this._db.schema.hasTable(TABLENAME_SCOREBOARD).then(itHas => {
 			if (itHas) {
-				this.log("ok", `DB: we have table '${TABLENAME_SCOREBOARD}', can safely continue working with players scores`);
-				this.dbInitialized = this.dbInitialized | DBInitializationState.ScoreboardInitialized;
+				this._log("ok", `DB: we have table '${TABLENAME_SCOREBOARD}', can safely continue working with players scores`);
+				this._dbInitialized = this._dbInitialized | DBInitializationState.ScoreboardInitialized;
 				return;
 			}
-			this.log("warn", `DB: seems we don't have table '${TABLENAME_SCOREBOARD}' in database, going to create it right now`);
-			this.dbClient.schema.createTable(TABLENAME_SCOREBOARD, (tb) => {
+			this._log("warn", `DB: seems we don't have table '${TABLENAME_SCOREBOARD}' in database, going to create it right now`);
+			this._db.schema.createTable(TABLENAME_SCOREBOARD, (tb) => {
 				tb.string("user").notNullable();
 				tb.integer("exp").notNullable();
 				tb.integer("streak").notNullable();
 			}).catch(err => {
-				this.log("err", "DB: we can't prepare DB", err);
+				this._log("err", "DB: we can't prepare DB", err);
 			}).then(() => {
-				this.log("ok", "DB: we successfully prepared our scoreboard table");
-				this.dbInitialized = this.dbInitialized | DBInitializationState.ScoreboardInitialized;
+				this._log("ok", "DB: we successfully prepared our scoreboard table");
+				this._dbInitialized = this._dbInitialized | DBInitializationState.ScoreboardInitialized;
 			});
 		});
-
-		this.countRegex = /^\d{0,}$/i;
 
 		let cid: NodeJS.Timer; let runs = 0;
 		cid = setInterval(() => {
 			runs++;
-			if (this.dbInitialized === DBInitializationState.FullyInitialized) {
-				this.log("ok", "DB is initialized");
+			if (this._dbInitialized === DBInitializationState.FullyInitialized) {
+				this._log("ok", "DB is initialized");
 				clearInterval(cid);
 			} else {
 				if (runs >= 10) {
 					clearInterval(cid);
-					this.log("err", "Timeout: waiting for DB initialization");
+					this._log("err", "Timeout: waiting for DB initialization");
 				}
 				return;
 			}
-			this.log("info", "Updating scoreboard messages");
-			this.updateScoreboardMessages();
+			this._log("info", "Updating scoreboard messages");
+			this._updateScoreboardMessages();
 		}, 1000);
 	}
 
-	async firstTime() {
+	private async _firstTimeBoot() {
 		const elem = {
 			date: Date.now(),
 			count: 1322,
@@ -170,9 +168,9 @@ class CountV2 extends Plugin implements IModule {
 		}
 
 		try {
-			await this.dbClient(TABLENAME_MAIN).insert(elem);
+			await this._db(TABLENAME_MAIN).insert(elem);
 		} catch (err) {
-			this.log("err", "First start: Can't but element into database", err);
+			this._log("err", "First start: Can't but element into database", err);
 		}
 
 		ch.send("**Первый запуск!**\n__Число__: 1322.\n__Далее__: **+15**");
@@ -180,8 +178,8 @@ class CountV2 extends Plugin implements IModule {
 		return true;
 	}
 
-	async onMessage(msg: Message) {
-		if (this.dbInitialized !== DBInitializationState.FullyInitialized) { return undefined; }
+	private async _onMessage(msg: Message) {
+		if (this._dbInitialized !== DBInitializationState.FullyInitialized) { return undefined; }
 		if (msg.channel.type === "dm") { return undefined; } // never reply in direct messages
 		if (msg.channel.id !== CHANNELID_MAIN) { return undefined; }
 		if (!msg.author) { msg.delete(); return undefined; }
@@ -189,13 +187,13 @@ class CountV2 extends Plugin implements IModule {
 		if (msg.author.id === $discordBot.user.id) { return undefined; }
 
 		const override = msg.content.startsWith("!");
-		if (!this.countRegex.test(override ? msg.content.slice(1) : msg.content)) { return msg.delete(); }
+		if (!CountV2._countRegex.test(override ? msg.content.slice(1) : msg.content)) { return msg.delete(); }
 
 		if (override && msg.author.id === $botConfig.botOwner) {
 			msg.react("⏳");
 			const nNumber = parseInt(msg.content.slice("!".length), 10);
 			try {
-				await this.dbClient(TABLENAME_MAIN).insert({
+				await this._db(TABLENAME_MAIN).insert({
 					date: Date.now(),
 					count: nNumber,
 					number: nNumber,
@@ -209,7 +207,7 @@ class CountV2 extends Plugin implements IModule {
 			} catch (err) {
 				msg.react("❌");
 				msg.channel.send(`❌ Ошибка перезаписи числа: \`${err.message}\`.`);
-				this.log("err", "Can't insert new number into database", err);
+				this._log("err", "Can't insert new number into database", err);
 			}
 			return;
 		} else if (override) {
@@ -220,9 +218,9 @@ class CountV2 extends Plugin implements IModule {
 
 		let latestRow: ICountOperationRow | undefined = undefined;
 		try {
-			latestRow = await this.dbClient(TABLENAME_MAIN).orderBy("date", "DESC").first("count", "author", "date", "operation", "number", "answered_by", "in_queue");
+			latestRow = await this._db(TABLENAME_MAIN).orderBy("date", "DESC").first("count", "author", "date", "operation", "number", "answered_by", "in_queue");
 		} catch (err) {
-			this.log("err", "Can't get latest row from database", err);
+			this._log("err", "Can't get latest row from database", err);
 			latestRow = undefined;
 		}
 
@@ -237,7 +235,7 @@ class CountV2 extends Plugin implements IModule {
 			try {
 				rRowAnsweredBy = JSON.parse(latestRow.answered_by);
 			} catch (err) {
-				this.log("err", "Can't parse latest row `answered_by` column");
+				this._log("err", "Can't parse latest row `answered_by` column");
 				return undefined;
 			}
 		} else {
@@ -245,7 +243,7 @@ class CountV2 extends Plugin implements IModule {
 		}
 
 		if (!rRowAnsweredBy) {
-			this.log("err", "No value for `rRowAnsweredBy` variable, returning...");
+			this._log("err", "No value for `rRowAnsweredBy` variable, returning...");
 			return;
 		}
 
@@ -272,15 +270,15 @@ class CountV2 extends Plugin implements IModule {
 
 			if (nNumber !== rRowNumber) {
 				setTimeout(async () => {
-					const r = await this.giveXP(msg.member, XPOperation.Lower);
+					const r = await this._giveXP(msg.member, XPOperation.Lower);
 					await msg.react("❌");
-					if (r) { this.updateScoreboardMessages(r); }
+					if (r) { this._updateScoreboardMessages(r); }
 				}, qTime);
 			} else {
 				setTimeout(async () => {
-					const r = await this.giveXP(msg.member, XPOperation.Raise);
+					const r = await this._giveXP(msg.member, XPOperation.Raise);
 					await msg.react("✅");
-					if (r) { this.updateScoreboardMessages(r); }
+					if (r) { this._updateScoreboardMessages(r); }
 				}, qTime);
 			}
 		}
@@ -299,7 +297,7 @@ class CountV2 extends Plugin implements IModule {
 				nextNumber += operation === "+" ? diffNumber : -Math.abs(diffNumber);
 
 				try {
-					await this.dbClient(TABLENAME_MAIN).insert({
+					await this._db(TABLENAME_MAIN).insert({
 						date: Date.now(),
 						count: rRowNumber,
 						number: nextNumber,
@@ -309,7 +307,7 @@ class CountV2 extends Plugin implements IModule {
 						in_queue: "-1"
 					});
 				} catch (err) {
-					this.log("err", "Can't put element into database", err);
+					this._log("err", "Can't put element into database", err);
 					msg.channel.send(":frowning: К сожалению, возникли проблемы с базой данных.");
 					return;
 				}
@@ -325,7 +323,7 @@ class CountV2 extends Plugin implements IModule {
 		}
 
 		try {
-			await this.dbClient(TABLENAME_MAIN).where({
+			await this._db(TABLENAME_MAIN).where({
 				date: latestRow.date,
 				number: latestRow.number
 			}).update(latestRow);
@@ -333,24 +331,24 @@ class CountV2 extends Plugin implements IModule {
 				msg.react("👁");
 			}
 		} catch (err) {
-			this.log("err", "Can't update element in database");
+			this._log("err", "Can't update element in database");
 			if (t) {
-				this.log("err", "Timer should not be called, clearing...");
+				this._log("err", "Timer should not be called, clearing...");
 				clearTimeout(t);
 			}
 			return undefined;
 		}
 	}
 
-	async giveXP(member: GuildMember, xpOperation: XPOperation): Promise<IScoreboardUserUpdateInfo | undefined> {
+	private async _giveXP(member: GuildMember, xpOperation: XPOperation): Promise<IScoreboardUserUpdateInfo | undefined> {
 		let userRow: IScoreboardUserRow | undefined = undefined;
 
 		try {
-			userRow = await this.dbClient(TABLENAME_SCOREBOARD).where({
+			userRow = await this._db(TABLENAME_SCOREBOARD).where({
 				user: member.id
 			}).first("user", "exp", "streak");
 		} catch (err) {
-			this.log("warn", "Can't poll user out'a DB", err);
+			this._log("warn", "Can't poll user out'a DB", err);
 			userRow = undefined;
 		}
 
@@ -361,9 +359,9 @@ class CountV2 extends Plugin implements IModule {
 				streak: 0
 			};
 			try {
-				await this.dbClient(TABLENAME_SCOREBOARD).insert(userRow);
+				await this._db(TABLENAME_SCOREBOARD).insert(userRow);
 			} catch (err) {
-				this.log("err", "Can't insert new user row to database", err);
+				this._log("err", "Can't insert new user row to database", err);
 				return undefined;
 			}
 		}
@@ -401,11 +399,11 @@ class CountV2 extends Plugin implements IModule {
 			} else { pointsGain = 0; }
 
 			try {
-				await this.dbClient(TABLENAME_SCOREBOARD).where({
+				await this._db(TABLENAME_SCOREBOARD).where({
 					user: userRow.user
 				}).update(userRow);
 			} catch (err) {
-				this.log("err", "Can't update element in database", err);
+				this._log("err", "Can't update element in database", err);
 				return undefined;
 			}
 
@@ -420,7 +418,7 @@ class CountV2 extends Plugin implements IModule {
 		}
 	}
 
-	async newScoreboardMessage() {
+	private async _newScoreboardMessage() {
 		if (!$discordBot.channels.has(CHANNELID_SCOREBOARD)) {
 			throw new Error("Scoreboard channel not found");
 		}
@@ -435,25 +433,25 @@ class CountV2 extends Plugin implements IModule {
 			const puprose = message.embeds[0].footer.text;
 			switch (puprose) {
 				case STRINGS.TOP_10: {
-					this.scoreboardMessages.top10 = message;
+					this._scoreboardMessages.top10 = message;
 				} break;
 				case STRINGS.LATEST_CHANGES: {
-					this.scoreboardMessages.latestChanges = message;
+					this._scoreboardMessages.latestChanges = message;
 				} break;
 				default: break;
 			}
 		}
 
-		if (!this.scoreboardMessages.top10) {
+		if (!this._scoreboardMessages.top10) {
 			const msg = <Message> await ch.send("", {
 				embed: generateEmbed(EmbedType.Empty, STRINGS.LOADING, {
 					footerText: STRINGS.TOP_10
 				})
 			});
-			this.scoreboardMessages.top10 = msg;
+			this._scoreboardMessages.top10 = msg;
 		}
 
-		if (this.scoreboardMessages.latestChanges) { return; }
+		if (this._scoreboardMessages.latestChanges) { return; }
 
 		const msg = <Message> await ch.send("", {
 			embed: generateEmbed(EmbedType.Empty, STRINGS.LOADING, {
@@ -461,22 +459,22 @@ class CountV2 extends Plugin implements IModule {
 			})
 		});
 
-		this.scoreboardMessages.latestChanges = msg;
+		this._scoreboardMessages.latestChanges = msg;
 	}
 
-	async updateScoreboardMessages(playerUpdate?: IScoreboardUserUpdateInfo) {
-		if (!this.scoreboardMessages.latestChanges || !this.scoreboardMessages.top10) {
+	private async _updateScoreboardMessages(playerUpdate?: IScoreboardUserUpdateInfo) {
+		if (!this._scoreboardMessages.latestChanges || !this._scoreboardMessages.top10) {
 			try {
-				this.log("info", "Probably cache was purged or plugin just started working, fetching messages from channel...");
-				await this.newScoreboardMessage();
+				this._log("info", "Probably cache was purged or plugin just started working, fetching messages from channel...");
+				await this._newScoreboardMessage();
 			} catch (err) {
-				this.log("err", "Can't update scoreboard messages, can't update scoreboard.", err);
+				this._log("err", "Can't update scoreboard messages, can't update scoreboard.", err);
 				return;
 			}
 		}
 
-		if (this.scoreboardMessages.latestChanges && playerUpdate) {
-			const lines = this.scoreboardMessages.latestChanges.embeds[0].description.split("\n").filter(l => l !== STRINGS.LOADING);
+		if (this._scoreboardMessages.latestChanges && playerUpdate) {
+			const lines = this._scoreboardMessages.latestChanges.embeds[0].description.split("\n").filter(l => l !== STRINGS.LOADING);
 			if (lines.length === 10) {
 				lines.splice(0, 1); // adding one line
 			}
@@ -491,18 +489,18 @@ class CountV2 extends Plugin implements IModule {
 			embed.description = lines.join("\n");
 			embed.footer = { text: STRINGS.LATEST_CHANGES };
 
-			await this.scoreboardMessages.latestChanges.edit("", {
+			await this._scoreboardMessages.latestChanges.edit("", {
 				embed: embed
 			});
 		}
 
-		if (!this.scoreboardMessages.top10) { return; } 
+		if (!this._scoreboardMessages.top10) { return; } 
 
 		let top10: IScoreboardUserRow[];
 		try {
-			top10 = await this.dbClient(TABLENAME_SCOREBOARD).orderBy("exp", "DESC").limit(15);
+			top10 = await this._db(TABLENAME_SCOREBOARD).orderBy("exp", "DESC").limit(15);
 		} catch (err) {
-			this.log("err", "Can't get top 10 from database!", err);
+			this._log("err", "Can't get top 10 from database!", err);
 			return;
 		}
 
@@ -512,7 +510,7 @@ class CountV2 extends Plugin implements IModule {
 			if (row.exp < 10) { continue; }
 			if (pos >= 10) { continue; }
 
-			const member = this.scoreboardMessages.top10.guild.members.get(row.user);
+			const member = this._scoreboardMessages.top10.guild.members.get(row.user);
 			if (!member) { continue; }
 
 			pos++;
@@ -526,12 +524,12 @@ class CountV2 extends Plugin implements IModule {
 		embed.description = lines.join("\n");
 		embed.footer = { text: STRINGS.TOP_10 };
 
-		await this.scoreboardMessages.top10.edit("", {
+		await this._scoreboardMessages.top10.edit("", {
 			embed: embed
 		});
 	}
 
-	async unload() {
+	public async unload() {
 		this.unhandleEvents();
 		return true;
 	}
