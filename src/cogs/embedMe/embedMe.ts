43d49320e17a90615c2d1df06c1b479275187dc6
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-05-27 19:37:19 +0700

    embedMe: use getMessageMemberOrAuthor embedMe: return promise of message sending embedMe: await message deletion

--- src/cogs/embedMe/embedMe.ts
+++ src/cogs/embedMe/embedMe.ts
@@ -3,6 +3,7 @@ import { Plugin } from "../plugin";
 import { Message } from "discord.js";
 import { command as docCmd } from "../utils/help";
 import { localizeForUser } from "../utils/ez-i18n";
+import { getMessageMemberOrAuthor, getUserDisplayName } from "@cogs/utils/utils";
 
 @docCmd("HELPFUL", "embed", "loc:EMBEDME_CMDMETA_DESCRIPTION", {
 	"loc:PROFILES_META_SETBIO_ARG0": {
@@ -22,38 +23,40 @@ class EmbedME extends Plugin implements IModule {
 	}
 
 	private async _onMessage(msg: Message) {
-		if (msg.author && msg.author.bot) { return; }
 		if (!msg.content.startsWith("!embed")) { return; }
+
+		const msgAuthor = await getMessageMemberOrAuthor(msg);
+		if (!msgAuthor) { return; }
+
 		if (msg.content === "!embed") {
-			const str = await localizeForUser(msg.member, "EMBEDME_INFO");
-			msg.channel.send(`:information_source: ${str}`);
-			return;
+			const str = await localizeForUser(msgAuthor, "EMBEDME_INFO");
+			return msg.channel.send(`:information_source: ${str}`);
 		}
 
-		let mContent = msg.content.slice("!embed ".length);
+		let embedContent = msg.content.slice("!embed ".length);
 
-		if (mContent.startsWith("`") && mContent.endsWith("`")) {
-			mContent = mContent.slice(1).substring(0, mContent.length - 2);
+		if (embedContent.startsWith("`") && embedContent.endsWith("`")) {
+			embedContent = embedContent.slice(1).substring(0, embedContent.length - 2);
 		}
 
 		await msg.channel.send("", {
 			embed: {
 				author: {
 					icon_url: msg.author.avatarURL({ format: "webp", size: 128 }),
-					name: msg.member ? msg.member.displayName : msg.author.username
+					name: getUserDisplayName(msgAuthor)
 				},
-				description: mContent,
+				description: embedContent,
 				timestamp: msg.createdAt,
 				footer: {
 					icon_url: $discordBot.user.displayAvatarURL({ format: "webp", size: 128 }),
-					text: await localizeForUser(msg.member, "EMBEDME_EMBED", {
+					text: await localizeForUser(msgAuthor, "EMBEDME_EMBED", {
 						botName: $discordBot.user.username
 					})
 				}
 			},
 		});
 
-		if (msg.channel.type === "text") { msg.delete(); }
+		if (msg.channel.type === "text") { await msg.delete(); }
 	}
 
 	public async unload() {
