af345af69d8caef11d974cae730c0a82729b8215
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-07-16 14:30:10 +0700

    gulp: copy docker files in "necessary-copying" task gulp: remove useless "./" from "lint" task tslint path

--- gulpfile.js
+++ gulpfile.js
@@ -20,17 +20,39 @@ gulp.task("compile", () => {
 });
 
 gulp.task("necessary-copying", () => {
-    return gulp.src([ "./package{,-lock}.json", "./src/**/*.*", "!./src/**/*.{ts,md}", "!./src/**/.git/*" ])
-        .pipe(gulp.dest(`${destFolder}/`));
+    return Promise.all([
+        gulp
+            .src([
+                "package{,-lock}.json",
+                "src/**/*.*",
+                "!src/**/*.{ts,md}",
+                "!src/**/.git/*",
+                "docker/{.,}[Dd]ocker*{,.*}"
+            ])
+            .pipe(gulp.dest(`${destFolder}/`)),
+        gulp
+            .src([
+                "docker/images/db/**/*{,.*}"
+            ])
+            .pipe(gulp.dest(`${destFolder}/.docker/db/`)),
+        gulp
+            .src([
+                "docker/images/bot/Dockerfile"
+            ])
+            .pipe(gulp.dest(`${destFolder}/`))
+    ]);
 });
 
-gulp.task("build", gulp.series(["compile", "necessary-copying"]));
+gulp.task("build", gulp.series([
+    "compile",
+    "necessary-copying"
+]));
 
 gulp.task("lint", () => {
     const gulpTslint = require("gulp-tslint");
     const tslint = require("tslint");
 
-    const program = tslint.Linter.createProgram("./tsconfig.json");
+    const program = tslint.Linter.createProgram("tsconfig.json");
 
     return gulp.src("src/**/*.ts", { base: '.' })
         .pipe(gulpTslint({
