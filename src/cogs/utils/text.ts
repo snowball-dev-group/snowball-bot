e2ad7c51653a7c8430e539cff9a33d5e0e8d3055
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2018-07-30 11:18:16 +0700

    utils:text: add function to break important mentions utils:text: add region for UUID generation ƒ utils:text: add documentation for some ƒs

--- src/cogs/utils/text.ts
+++ src/cogs/utils/text.ts
@@ -12,24 +12,49 @@ export function escapeRegExp(str: string) {
 
 export function replaceAll(str: string, search: string, replacement: string) {
 	search = escapeRegExp(search);
+
 	return str.replace(new RegExp(search, "g"), replacement);
 }
 
+/**
+ * Reverse the string
+ * @param str String to reverse
+ */
 export function reverseString(str: string) {
 	let o = "";
 	for (let i = str.length; i > -1; i--) { o += str[i]; }
+
 	return o;
 }
 
+/**
+ * Can string be the snowflake ID (string contains 16-20 digits only)
+ * @param str String to check for snowflake
+ */
 export function canBeSnowflake(str: string) {
 	return /[0-9]{16,20}/.test(str);
 }
 
+/**
+ * Strip multiple empty characters from the string
+ * @param str String to strip spaces from
+ */
 export function stripSpaces(str: string) {
 	return str.trim().replace(/ {2,}/g, " ");
 }
 
-// from https://github.com/danakt/uuid-by-string/blob/master/uuid-by-string.js
+/**
+ * Break important Discord mentions such as @everyone and @here
+ * @param str String to break mentions in
+ * @param breaker String used to break mentions (inserted between "@" and "everyone")
+ */
+export function removeEveryoneMention(str: string, breaker = "\u200B") {
+	return str
+		.replace("@everyone", `@${breaker}everyone`)
+		.replace("@here", `@${breaker}here`);
+}
+
+// #region UUID Generation
 
 function _getHex(str: string, key: number, maxlen: number): string {
 	let n: number, i: number, count: number;
@@ -46,6 +71,7 @@ function _getHex(str: string, key: number, maxlen: number): string {
 		n = Number(String(n).replace(/0+$/g, ""));
 		while (n.toString(16).length > maxlen) { n = Math.floor(n / 10); }
 	}
+
 	return n.toString(16);
 }
 
@@ -56,9 +82,15 @@ function _makeUUID(p: string[]) {
 		4 + p[1].substr(4, 3), (Number(`"0x"${p[1][7]}`) & 0x3 | 0x8).toString(16) + p[1].substr(8, 3),
 		p[2]
 	];
+
 	return s.join("-").toUpperCase();
 }
 
+/**
+ * Generate UUID using the string
+ * @param str String that used to generate UUID
+ * @link https://github.com/danakt/uuid-by-string/blob/master/uuid-by-string.js
+ */
 export function getUUIDByString(str: string) {
 	return _makeUUID([
 		_getHex(str, 0xf6, 8),
@@ -67,8 +99,13 @@ export function getUUIDByString(str: string) {
 	]);
 }
 
-// from https://stackoverflow.com/questions/11305797/remove-zero-width-space-characters-from-a-javascript-string/11305926
-// may improve in time
+// #endregion
+
+/**
+ * Strip zero width spaces from the string
+ * @param str String to strip empty characters from\
+ * @link https://stackoverflow.com/questions/11305797/remove-zero-width-space-characters-from-a-javascript-string/11305926
+ */
 export function stripEmptyChars(str: string) {
 	return str.replace(/[\u200B-\u200D\uFEFF]/g, "");
 }
